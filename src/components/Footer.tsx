0bbd55bb18a168667ebf1b80d22d152b32b1d737
Gaëlle CHARLES-BELAMOUR <54911761+Gaelle-charles@users.noreply.github.com>
2025-09-17 18:01:05 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <footer className="py-8 bg-white text-white rounded-t-[4rem] -mt-16 z-50 relative">
         <div className="max-w-7xl mx-auto px-8 text-center">
             <div className="flex items-center justify-center space-x-2 mb-4">
-              <img src="https://zsvnqforlvunxzphatey.supabase.co/storage/v1/object/public/Images/Group%209.png" src="logo"/>
+              <img src="https://zsvnqforlvunxzphatey.supabase.co/storage/v1/object/public/Images/Group%209.png" alt="logo"/>
             </div>
 
               {/* Social Links */}

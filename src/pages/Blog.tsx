5e07d294267e6dfd249f3a5e3cf9e1a826c149a6
Gaëlle CHARLES-BELAMOUR <54911761+Gaelle-charles@users.noreply.github.com>
2025-09-19 13:54:27 -0400

    Update Blog.tsx

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -80,7 +80,7 @@ const Blog = () => {
                       alt={featuredArticle.title}
                       className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
                       onError={(e) => {
-                        e.currentTarget.src = "/lovable-uploads/3a267165-a774-4ac0-a1ab-b489ef8f5bd0.png";
+                        e.currentTarget.src = "/lovable-uploads/Gemini_Generated_Image_w3mr83w3mr83w3mr.png";
                       }}
                     />
                   </div>

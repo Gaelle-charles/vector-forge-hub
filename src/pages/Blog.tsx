208334efc1a84e09367bf05a1f557ca9cbbcabf1
Gaëlle CHARLES-BELAMOUR <54911761+Gaelle-charles@users.noreply.github.com>
2025-09-19 13:26:54 -0400

    Update Blog.tsx

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -31,9 +31,9 @@ const Blog = () => {
         </div>
         
         <div className="relative z-10 max-w-7xl mx-auto py-4 sm:px-6 lg:px-8 text-center">
-          <h1 className="text-5xl md:text-6xl font-bold mb-6 text--[#e76f51] drop-shadow-lg">Blog
+          <h1 className="text-5xl md:text-6xl font-bold mb-6 text-black drop-shadow-lg">Blog
           </h1>
-          <p className="text-xl text--[#e76f51]/90 max-w-3xl mx-auto mb-12 drop-shadow-md">
+          <p className="text-xl text-black/90 max-w-3xl mx-auto mb-12 drop-shadow-md">
             Découvrez nos articles et plongez dans l'univers de l'intelligence artificielle émergentes qui vous permettra de booster votre business.
           </p>
         </div>
